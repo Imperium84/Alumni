e0849f6f8001358b68813ac11811c617529bf75d
Antoine Cornwell <cornwell.a.euroa2016@gmail.com>
2019-10-04 13:58:51 +0200

    avatar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,28 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+//use App\Controller\AvatarController;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home.index")
+     * @Route("/", name="index")
      */
     public function index()
     {
         return $this->render("home.html.twig");
     }
+
+    /**
+     * @Route("/requete", name="requetes")
+     */
+    public function requete($uploadDir)
+    {
+        return new Response("tocard");
+    }
+
 
 
 }
